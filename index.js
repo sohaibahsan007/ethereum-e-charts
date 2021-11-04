83e2bbff1bd43188aad9aff2d8fb65b2f54d83f0
Sohaib Ahsan <sohaibahsan007@live.com>
2021-11-04 18:31:08 +0500

    UPdates

--- index.js
+++ index.js
@@ -1,88 +1,219 @@
-      // Initialize the echarts instance based on the prepared dom
-      var myChart = echarts.init(document.getElementById('main'),'dark');
+// Initialize the echarts instance based on the prepared dom
+var myChart = echarts.init(document.getElementById("main"), "dark");
 
-      $.get('data.csv', function (csvStr) {
-          const data = Papa.parse(csvStr).data?.slice(1).sort(function(a,b){
-            return Number(new Date(a[0])) - Number(new Date(b[0]));
-           })?.filter(d => d[0]);
-           const data1 = JSON.parse(JSON.stringify(data))?.map(d => {
-            d[0] = d[0];
-            delete d[2];
-            return [d[0],d[1]];
-          });
-          const data2 =  JSON.parse(JSON.stringify(data))?.map(d => {
-            d[0] = d[0];
-            delete d[1];
-            return [d[0],d[2]];
-          });
-            // Specify the configuration items and data for the chart
-          var option = {
-            color: ['#4172C3','#FF990B'],
-            xAxis: {
-              type: 'category',
+$.get("data.csv", function (csvStr) {
+  // Specify the configuration items and data for the chart
+  const data = parseDataByColumns(csvStr);
+  const dateList = JSON.parse(JSON.stringify(data))?.map((d) => d?.Date);
+  const supplyList = JSON.parse(JSON.stringify(data))?.map((d) =>
+    parseInt(d?.Supply?.replaceAll(",", ""))
+  );
+  const rateList = JSON.parse(JSON.stringify(data))?.map((d) =>
+    parseInt(d?.["Issuance Rate"]?.replaceAll("%", ""))
+  );
+  var option = {
+    title: {
+      text: "Ethereum's Historical and Projected Issuance Rate",
+      textAlign: 'center',
+      textVerticalAlign: 'auto',
+      left: '50%',
+      top: '4%'
+    },
+    color: ["#4172C3", "#FF990B"],
+    xAxis: {
+      type: "category",
+      data: dateList,
+    },
+    tooltip: {
+      trigger: "axis",
+    },
+    legend: {
+      data: ["Supply", "Days"],
+      top: '10%'
+    },
+    calculable: true,
+    grid: {
+      show: true,
+      top: '20%'
+    },
+    yAxis: [
+      {
+        type: "value",
+        name: "Supply",
+      },
+      {
+        type: "value",
+        name: "Days",
+        axisLabel: {
+          formatter: function (value, index) {
+            return value + "%";
+          },
+        },
+      },
+    ],
+    series: [
+      {
+        data: supplyList,
+        type: "line",
+        name: "Supply",
+        smooth: true,
+        smoothMonotone: "x",
+        symbol: "none",
+        yAxisIndex: 0,
+        emphasis: {
+          focus: "series",
+        },
+        lineStyle: {
+          width: 3,
+          color: "#4172C3",
+        },
+      },
+      {
+        data: rateList,
+        type: "line",
+        id: "line2",
+        name: "Days",
+        step: false,
+        smooth: false,
+        smoothMonotone: "x",
+        symbol: "none",
+        yAxisIndex: 1,
+        emphasis: {
+          focus: "series",
+        },
+        lineStyle: {
+          width: 3,
+          color: "#FF990B",
+        },
+        markPoint: {
+          data: [
+            {
+              yAxis: 13,
+              xAxis: "9/23/2015",
+              label: {
+                width: 100,
+                formatter: ["Homestead ", "(Block time increase)"].join("\n"),
+              },
+              symbolSize: [110, 40],
+              symbolOffset: [0, 10],
             },
-            tooltip: {
-              trigger: 'axis',
+            {
+              yAxis: 16,
+              xAxis: "6/20/2016",
+              label: {
+                width: 100,
+                formatter: ["DDOS"].join("\n"),
+              },
+              symbolSize: [40, 20],
+              symbolOffset: [0, -15],
             },
-            calculable : true,
-            legend: {},
-            grid: {show: true},
-            yAxis: [
-              {
-                type: 'value',
-                name: 'Supply',
-                showGrid: true,
+            {
+              yAxis: 15,
+              xAxis: "10/27/2016",
+              label: {
+                width: 100,
+                formatter: ["Difficulty Bomb"].join("\n"),
               },
-              {
-                type: 'value',
-                name: 'Days',
-                showGrid: true,
-                max: function (value) {
-                  return value.max + 20;
-                },
-                axisLabel:{
-                  formatter: function (value, index) {
-                      return value+"%";
-                  },
+              symbolSize: [80, 20],
+              symbolOffset: [0, -15],
+            },
+            {
+              yAxis: 6,
+              xAxis: "10/21/2017",
+              label: {
+                width: 100,
+                formatter: ["Byzantium","5>3 reward drop + bomb delay"].join("\n"),
               },
-              }
-            ],
-            series: [
-              {
-                data: data1,
-                type: 'line',
-                smooth: true,
-                symbol: 'none',
-                yAxisIndex: 0,
-                lineStyle: {
-                  width: 3,
-                  color: '#4172C3'
-                }
+              symbolSize: [80, 20],
+              symbolOffset: [0, 25],
+            },
+            {
+              yAxis: 8,
+              xAxis: "5/17/2018",
+              label: {
+                width: 100,
+                formatter: ["Difficulty Bomb"].join("\n"),
               },
-              {
-                data: data2,
-                type: 'line',
-                smooth: true,
-                symbol: 'none',
-                yAxisIndex: 1,
-                lineStyle: {
-                  width: 3,
-                  color: '#FF990B'
-                },
-                markPoint: {
-                  data: [
-                    { type: 'max', name: 'Max' },
-                    { type: 'min', name: 'Min' }
-                  ]
-                },
-                markLine: {
-                  data: [{ type: 'average', name: 'Avg' }]
-                }
+              symbolSize: [80, 20],
+              symbolOffset: [0, -15],
+            },
+            {
+              yAxis: 7,
+              xAxis: "12/11/2018",
+              label: {
+                width: 100,
+                formatter: ["Constantinople","3 > 2 reward drop + bomb delay"].join("\n"),
               },
-            ]
-          };
+              symbolSize: [80, 20],
+              symbolOffset: [-40, 60],
+            },
+            {
+              yAxis: 5,
+              xAxis: "10/22/2019",
+              label: {
+                width: 100,
+                formatter: ["Difficulty Bomb"].join("\n"),
+              },
+              symbolSize: [80, 20],
+              symbolOffset: [0, 55],
+            },
+            {
+              yAxis: 5,
+              xAxis: "11/29/2019",
+              label: {
+                width: 100,
+                formatter: ["Muir Glacier","(Bomb delay)"].join("\n"),
+              },
+              symbolSize: [80, 20],
+              symbolOffset: [0, -45],
+            },
+            {
+              yAxis: 5,
+              xAxis: "7/27/2020",
+              label: {
+                width: 100,
+                formatter: ["Eth2 Phase 0*","(Beacon Chain)"].join("\n"),
+              },
+              symbolSize: [80, 20],
+              symbolOffset: [0, 5],
+            },
+            {
+              yAxis: 6,
+              xAxis: "4/20/2021",
+              label: {
+                width: 100,
+                formatter: ["Eth2 Phase 1.5*","(Eth1 folded into Eth2)"].join("\n"),
+              },
+              symbolSize: [80, 20],
+              symbolOffset: [0, -15],
+            },
+          ],
+          label: {
+            show: true,
+            fontSize: 10,
+            overflow: "breakAll",
+          },
+          itemStyle: {
+            color: "#333333",
+            opacity: 0.8,
+          },
+          symbol: "roundRect",
+        },
+      },
+    ],
+  };
 
-      // Display the chart using the configuration items and data just specified.
-      myChart.setOption(option);
-      });
-
+  // Display the chart using the configuration items and data just specified.
+  myChart.setOption(option);
+});
+function parseDataByColumns(csvStr) {
+  const rawData = Papa.parse(csvStr, {
+    header: true,
+  }).data;
+  const data = rawData
+    ?.sort(function (a, b) {
+      return Number(new Date(a?.Date)) - Number(new Date(b?.Date));
+    })
+    ?.filter((d) => d?.Date);
+  return data;
+}
